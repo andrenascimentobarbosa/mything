377f4a41ad18aa057f43bcf767eb6c43c34fcc6c
andrenascimentobarbosa <andrebarbosa2024@gmail.com>
2024-04-30 22:01:44 -0300

    changes

--- password_generatorgui.py
+++ password_generatorgui.py
@@ -4,32 +4,31 @@ import tkinter as tk
 from random import shuffle
 import string
 
+
 def generate_password(length=12):
-    letters = string.ascii_letters  # Includes lowercase and uppercase letters
+    letters = string.ascii_letters
     digits = string.digits
     punctuation = string.punctuation
-
-    # Combine all character sets into one list
+    
     characters = list(letters + digits + punctuation)
 
-    # Shuffle the characters to randomize their order
+    
     shuffle(characters)
 
-    # Generate a random password by picking characters randomly from the shuffled list
     password = ''.join(characters[:length])
     
     return password
+
 
 def generate_password_gui():
     length = int(length_entry.get())
     password = generate_password(length)
     result_label.config(text=password)
 
-# Create the main window
-window = tk.Tk()
-window.title("Random Password Generator")
 
-# Create and place widgets in the window
+window = tk.Tk()
+window.title('p4$sw0rd-g3nr4t0r')
+
 length_label = tk.Label(window, text="Password Length:")
 length_label.pack()
 
@@ -39,9 +38,9 @@ length_entry.pack()
 generate_button = tk.Button(window, text="Generate Password", command=generate_password_gui)
 generate_button.pack()
 
-result_label = tk.Label(window, text="Generated Password will appear here")
+result_label = tk.Label(window, text="************")
 result_label.pack()
 
-# Start the GUI event loop
+
 window.mainloop()
 
